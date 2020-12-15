ef0cc5b6c6e749dc5e9da947907f6af98c9b7208
Muusann <73873796+Muusann@users.noreply.github.com>
2020-12-15 15:29:01 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def items_params
+    params.require(:items).permit(:image, :name, :category_id, :condition_id, :description, :price, :delivery_date_id, :delivery_fee_id, :prefecture_id).merge(user_id: current_user.id)
   end
 end
