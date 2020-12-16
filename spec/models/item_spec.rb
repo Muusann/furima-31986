7ce66eaffa7fa84b121c139495b17640f541624b
Muusann <73873796+Muusann@users.noreply.github.com>
2020-12-16 14:46:22 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
       end
       it '価格が半角英数混合だと出品できない' do
-        @item.price = "550a"
+        @item.price = "3a0"
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
@@ -77,6 +77,11 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
       end
       it '価格が半角数字でないと出品できない' do
+        @item.price = "３００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it '価格が全角だと登録できない' do
         @item.price = "３００"
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
