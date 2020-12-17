a0324f0e03b0dcd8da283b507cfaaead0a07b400
Muusann <73873796+Muusann@users.noreply.github.com>
2020-12-17 21:42:27 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
     if current_user.id != @item.user_id
-    redirect_to item_path
+    redirect_to root_path
     end
   end
   
