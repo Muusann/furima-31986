e3a459f4088a18207f8456cbb348702f954623d7
Muusann <73873796+Muusann@users.noreply.github.com>
2020-12-23 18:33:42 +0900

    commit

--- spec/requests/purchas_request_spec.rb
+++ spec/requests/purchas_request_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe "Purchas", type: :request do
   before do
     @purcha = FactoryBot
   end
+
   describe "GET /index" do
     it "returns http success" do
       get "/purchas/index"
       expect(response).to have_http_status(:success)
     end
   end
-
+end
